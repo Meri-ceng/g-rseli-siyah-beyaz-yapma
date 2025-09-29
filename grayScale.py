5c89855e63bbf7b99426941c6bf38db99476fff3
MERO <meryemakarsu1301@gmail.com>
2025-09-29 17:26:59 +0300

    Update grayScale.py

--- grayScale.py
+++ grayScale.py
@@ -1,17 +1,14 @@
 import cv2
 
-# Görseli oku
-resim = cv2.imread("images/cilek.jpg")  # images klasörüne koyduğun görseli okur
+resim = cv2.imread("images/cilek.jpg")  
 
-# Siyah-beyaz (grayscale) yap
 gri_resim = cv2.cvtColor(resim, cv2.COLOR_BGR2GRAY)
 
-# Gri resmi kaydet
-cv2.imwrite("images/cilek_gri.jpg", gri_resim)
+cv2.imwrite("images/cilek_gri.jpg", gri_resim) #yeni ve gri resmimi kaydettim
 
 # Görselleri göster
 cv2.imshow("Orijinal", resim)
 cv2.imshow("Siyah-Beyaz", gri_resim)
 
-cv2.waitKey(0)            # pencereyi açık tut
-cv2.destroyAllWindows()   # tüm pencereleri kapat
+cv2.waitKey(0)           
+cv2.destroyAllWindows() 
